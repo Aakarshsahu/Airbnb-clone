028a638812daf227cf1b5ae8cf73ad1d53e7bcb4
Aakarshsahu <aakarshsshu2@gmail.com>
2023-03-04 11:22:59 +0530

    fixes/mongo-uri-change-04032023

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 const plm = require("passport-local-mongoose");
 const MONGO_URL = "mongodb+srv://aakarshsahu:Aakarsh@#$1437@cluster0.wapzcve.mongodb.net/?retryWrites=true&w=majority"
-mongoose.connect(MONGO_URL , { useNewUrlParser: true });
+try {
+  await mongoose.connect(MONGO_URL, { useNewUrlParser: true });
+} catch (error) {
+  console.error(error);
+}
 
 
 const userSchema = mongoose.Schema({
